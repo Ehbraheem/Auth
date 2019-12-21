9e5ba420439e796522c11b4b1058d821bae3277c
Ehbraheem <ehbraheem@gmail.com>
2019-12-21 14:45:51 +0100

    Added sample payload verification spec

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,7 +1,16 @@
 RSpec.describe "Auth Api" do
-  it 'successfully pings app\'s root' do
-    get "/"
+  before(:each) { get "/" }
 
+  it 'successfully pings app\'s root' do
     expect(last_response).to be_ok
+  end
+
+  it "returns correct response body" do
+    payload = parsed_body
+
+    puts payload
+    expect(payload).to have_key "welcome"
+    expect(payload["welcome"]).to_not be_nil
+    expect(payload["welcome"]).to eq "This is an authn/authz service"
   end
 end
