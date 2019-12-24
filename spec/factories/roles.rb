f66c2461e1ff207e434de817b9ed9144416df609
Ehbraheem <ehbraheem@gmail.com>
2019-12-25 00:15:22 +0100

    Added factory for Role

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :role do
     name { Faker::Job.position }
     description { Faker::Lorem.paragraphs }
     uuid { Faker::Internet.uuid }
 
-
     trait :with_parent do
       parent { association self }
     end
   end
-  
 end
