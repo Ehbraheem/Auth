996ccb7352d279e7ecd01eb3293a25d3115fc814
Ehbraheem <ehbraheem@gmail.com>
2019-12-25 00:58:21 +0100

    Fleshed out role spec

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,13 +1,34 @@
 # frozen_string_literal: true
 
-RSpec.describe Role do
+RSpec.describe Role, type: :model do
   context 'Basic CRUD' do
-    it 'can create valid Role'
+    let(:roles) { FactoryBot.create_list :role, 10 }
+    let(:object) { FactoryBot.attributes_for :role }
 
-    it 'update existing Role'
+    it 'can create valid Role' do
+      obj = FactoryBot.create :role
+      expect(obj).to be_persisted
+    end
 
-    it 'can read existing Role'
+    it 'update existing Role' do
+      roles
+      obj = Role.first
+      expect(obj.update(object)).to be true
+    end
 
-    it 'can delete valid Role'
+    it 'can read existing Role' do
+      obj = Role.first
+      expect(Role.all).to_not be_empty
+      expect(Role.find(obj.id)).to be_a_kind_of Role
+    end
+
+    it 'can delete valid Role' do
+      obj = Role.first
+      expect(Role.find(obj.id)).to be_a_kind_of Role
+      Role.destroy(obj.id)
+      expect do
+        Role.find(obj.id)
+      end    .to raise_exception Mongoid::Errors::DocumentNotFound
+    end
   end
 end
