cc5385308432a83c8019f0353800f540c217a8ab
Ehbraheem <ehbraheem@gmail.com>
2019-12-24 20:45:39 +0100

    Cleaned up boot file

--- boot.rb
+++ boot.rb
@@ -1,9 +1,11 @@
-$:.unshift File.expand_path(File.dirname(__FILE__))
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift __dir__
 
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-ENV['ROOT_PATH'] = "#{File.expand_path(File.dirname(__FILE__))}"
+ENV['ROOT_PATH'] = __dir__
 
 require 'config/environment'
 require 'app'
