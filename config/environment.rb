f85aeb7bcc31b87f24569dac74b7b255a762aeaa
Ehbraheem <ehbraheem@gmail.com>
2019-12-25 00:58:45 +0100

    Eager-load app files

--- config/environment.rb
+++ config/environment.rb
@@ -8,3 +8,6 @@ ENV['RACK_ENV'] ||= 'development'
 Bundler.require :default, ENV['RACK_ENV'].to_sym
 
 require_relative './initializers/mongoid'
+Dir[File.join(__dir__, '../app/**/*.rb')].each do |f|
+  require_relative f unless f[%r{^\./spec/}]
+end
