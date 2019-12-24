e391d79aae29a454638fec3f8c72ae649d7f7f34
Ehbraheem <ehbraheem@gmail.com>
2019-12-24 21:12:27 +0100

    Decoupled DB connect logic

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,4 @@ ENV['RACK_ENV'] ||= 'development'
 
 Bundler.require :default, ENV['RACK_ENV'].to_sym
 
-Sinatra::Base.configure :production, :development, :test do
-  # we are running this method from top-level
-
-  # Bootstrap MongoDB connection
-  Mongoid.load! './config/mongoid.yml'
-end
+require_relative './initializers/mongoid'
