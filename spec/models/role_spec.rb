231ff317c68caef67ba495746861583f90df8885
Ehbraheem <ehbraheem@gmail.com>
2019-12-24 23:57:45 +0100

    Added basic spec for Role model

---
+++ spec/models/role_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe Role do
+  context 'Basic CRUD' do
+    it 'can create valid Role'
+
+    it 'update existing Role'
+
+    it 'can read existing Role'
+
+    it 'can delete valid Role'
+  end
+end
