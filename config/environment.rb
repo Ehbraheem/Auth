6a0c24b2ee3a850f98c9fdb35f50ee26a5e0e21c
Ehbraheem <ehbraheem@gmail.com>
2019-12-24 20:54:26 +0100

    Sets up environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require 'rubygems'
+require 'bundler/setup'
+
+ENV['RACK_ENV'] ||= 'development'
+
+Bundler.require :default, ENV['RACK_ENV'].to_sym
+
 Sinatra::Base.configure :production, :development, :test do
   # we are running this method from top-level
 
