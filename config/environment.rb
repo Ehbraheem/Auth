580e62257198f60bd845d4152b734239aafe4624
Ehbraheem <ehbraheem@gmail.com>
2019-12-25 12:51:18 +0100

    Prepared application for extension

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,10 @@ ENV['RACK_ENV'] ||= 'development'
 Bundler.require :default, ENV['RACK_ENV'].to_sym
 
 require_relative './initializers/mongoid'
+
+# Makes base app class available to subclasses
+require_relative '../app'
+
 Dir[File.join(__dir__, '../app/**/*.rb')].each do |f|
   require_relative f unless f[%r{^\./spec/}]
 end
