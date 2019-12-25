09e694fa1224e627ce4d699ebfc4c88fbc6fbfca
Ehbraheem <ehbraheem@gmail.com>
2019-12-25 14:25:04 +0100

    Added test for show action

--- spec/controllers/roles_spec.rb
+++ spec/controllers/roles_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Roles, type: :controller do
 
   context 'GET #index' do
     let(:roles) { build_stubbed_list(:role, 5) }
-    
+
     it 'success and renders all roles' do
       allow(Role).to receive(:all).and_return(roles)
 
@@ -28,8 +28,32 @@ RSpec.describe Roles, type: :controller do
   end
 
   context 'GET #show' do
-    it 'success and renders role'
-    it 'fails because no role is associated with id'
+    let(:role) { build_stubbed(:role) }
+
+    before(:each) do
+      allow(Role).to receive(:find).with(role.id).and_return(role)
+      allow(Role).to receive(:find).with('sdvsvsADGFsbCvs').and_return(nil)
+    end
+    it 'success and renders role' do
+
+      get "/roles/#{role.id}"
+
+      payload = parsed_body
+      
+      role.each do |key, value|
+        expect(payload).to have_key key
+        expect(payload[key]).to_not be_nil
+        expect(payload[key]).to eq value
+      end
+    end
+
+    it 'fails because no role is associated with id' do
+      get "/roles/sdvsvsADGFsbCvs"
+
+      payload = parsed_body
+
+      expect(payload).to be_nil
+    end
   end
 
   context 'POST #create' do
