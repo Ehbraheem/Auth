8e8d619431df2b060ed592f78eb4f0dc468dd746
Ehbraheem <ehbraheem@gmail.com>
2019-12-25 15:18:46 +0100

    Fixed linting smells

--- spec/controllers/roles_spec.rb
+++ spec/controllers/roles_spec.rb
@@ -1,5 +1,6 @@
-RSpec.describe Roles, type: :controller do
+# frozen_string_literal: true
 
+RSpec.describe Roles, type: :controller do
   context 'GET #index' do
     let(:roles) { build_stubbed_list(:role, 5) }
 
@@ -9,7 +10,7 @@ RSpec.describe Roles, type: :controller do
       get '/roles'
 
       payload = parsed_body
-      
+
       expect(payload).to respond_to :length
       expect(payload.length).to eq 5
       expect(payload).to match_array roles
@@ -21,7 +22,7 @@ RSpec.describe Roles, type: :controller do
       get '/roles'
 
       payload = parsed_body
-      
+
       expect(payload).to respond_to :length
       expect(payload.length).to eq 0
     end
@@ -36,11 +37,10 @@ RSpec.describe Roles, type: :controller do
     end
 
     it 'success and renders role' do
-
       get "/roles/#{role.id}"
 
       payload = parsed_body
-      
+
       role.each do |key, value|
         expect(payload).to have_key key
         expect(payload[key]).to_not be_nil
@@ -49,7 +49,7 @@ RSpec.describe Roles, type: :controller do
     end
 
     it 'fails because no role is associated with id' do
-      get "/roles/sdvsvsADGFsbCvs"
+      get '/roles/sdvsvsADGFsbCvs'
 
       payload = parsed_body
 
@@ -64,7 +64,7 @@ RSpec.describe Roles, type: :controller do
 
       allow(Role).to receive(:create).with(params).and_return(stubbed)
 
-      expect(post '/roles', params: { role: params }).to eq stubbed
+      expect(post('/roles', params: { role: params })).to eq stubbed
     end
   end
 
@@ -73,17 +73,21 @@ RSpec.describe Roles, type: :controller do
     let(:params) { attributes_for(:role) }
 
     before(:each) do
-      # Role.stub_chain(:find, :update).with(role.id).with(params).and_return(role.merge(params))
-      allow(Role).to receive(:find).with(role.id).and_return(role)
-      allow(Role).to receive(:find).with('sdvsvsADGFsbCvs').and_return(nil)
-      allow(role).to receive(:update).with(params).and_return(role.merge(params))
+      # Role.stub_chain(:find, :update).with(role.id)
+      # .with(params).and_return(role.merge(params))
+      allow(Role).to receive(:find).with(role.id)
+                                   .and_return(role)
+      allow(Role).to receive(:find).with('sdvsvsADGFsbCvs')
+                                   .and_return(nil)
+      allow(role).to receive(:update).with(params)
+                                     .and_return(role.merge(params))
     end
 
     it 'updated role info' do
       put "/roles/#{role.id}", params: { role: params }
 
       payload = parsed_body
-      
+
       params.each do |key, value|
         expect(payload).to have_key key
         expect(payload[key]).to_not be_nil
@@ -92,7 +96,7 @@ RSpec.describe Roles, type: :controller do
     end
 
     it 'fails because no role is associated with id' do
-      put "/roles/sdvsvsADGFsbCvs", params: { role: params }
+      put '/roles/sdvsvsADGFsbCvs', params: { role: params }
 
       payload = parsed_body
 
@@ -104,15 +108,18 @@ RSpec.describe Roles, type: :controller do
     let(:role) { build_stubbed(:role) }
 
     before(:each) do
-      # Role.stub_chain(:find, :update).with(role.id).with(params).and_return(role.merge(params))
-      allow(Role).to receive(:find).with(role.id).and_return(role)
-      allow(Role).to receive(:find).with('sdvsvsADGFsbCvs').and_return(nil)
-      allow(role).to receive(:destroy).and_return(role.merge(nil)
+      # Role.stub_chain(:find, :update).with(role.id)
+      # .with(params).and_return(role.merge(params))
+      allow(Role).to receive(:find).with(role.id)
+                                   .and_return(role)
+      allow(Role).to receive(:find).with('sdvsvsADGFsbCvs')
+                                   .and_return(nil)
+      allow(role).to receive(:destroy)
+        .and_return(role.merge(nil))
     end
 
     it 'deleted role' do
       delete "/roles/#{role.id}"
-
     end
     it 'fails because no role is associated with id'
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,18 +41,15 @@ FactoryBot.reload
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-
   config.include RSpecMixin
 
   config.include FactoryBot::Syntax::Methods
 
   config.expect_with :rspec do |expectations|
-    
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
-    
     mocks.verify_partial_doubles = true
   end
 
