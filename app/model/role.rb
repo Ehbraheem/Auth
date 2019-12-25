3fbb06d08889102bbd4d07e5ae0a372eb3a93a11
Ehbraheem <ehbraheem@gmail.com>
2019-12-25 01:05:13 +0100

    Fleshed out role model attributes

--- app/model/role.rb
+++ app/model/role.rb
@@ -3,4 +3,11 @@
 # #
 class Role
   include Mongoid::Document
+
+  field :_id,  type: String, default: -> { id }
+	field :id, type: String
+  field :name, type: String
+  field :description, type: String
+  field :uuid, type: String
+  field :parent, type: self.class
 end
